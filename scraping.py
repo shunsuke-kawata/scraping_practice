09a9c60a022163e8e62b174b03bc114be068f333
川田隼輔 <kwt20238120@gmail.com>
2023-08-10 08:23:16 +0900

    update 関数の目的を変更

--- scraping.py
+++ scraping.py
@@ -1,31 +1,38 @@
 import requests
-from bs4 import BeautifulSoup
+import os
+list_int = [i for i in range(1,120,1)]
+def get_paper_titles(page):
+    print(page)
+    
+    url = f""
 
-def get_paper_titles(page,keywords):
-    delimiter="+"
-    keyword =delimiter.join(keywords)
-    url = f"https://scholar.google.co.jp/scholar?start={page}&q={keyword}"
-
+    print(url)
     # Google Scholarへのリクエストを送信し、HTMLを取得
     response = requests.get(url)
-    html = response.text
+    print(response)
 
-    # BeautifulSoupを使用してHTMLを解析
-    soup = BeautifulSoup(html, "html.parser")
+    # # BeautifulSoupを使用してHTMLを解析
+    # soup = BeautifulSoup(html, "html.parser")
 
-    # 論文タイトルを含む要素を検索
-    title_elements = soup.find_all("h3", class_="gs_rt")
+    # # 論文タイトルを含む要素を検索
+    # title_elements = soup.find_all("h3", class_="gs_rt")
+    # print(title_elements)
 
-    # タイトルのテキストを取得
-    titles = [title.text for title in title_elements]
+    # # タイトルのテキストを取得
+    # titles = [title.text for title in title_elements]
+    # print(titles)
 
-    return titles
 
 # キーワードを指定して論文タイトルを取得
 
 keywords =["orb","slam","frame"]
-for page in range(0,91,10):
-    print(f"start{page}")
-    paper_titles = get_paper_titles(page,keywords)
-    for title in paper_titles:
-        print(title)
+for page in range(1,120,1):
+    get_paper_titles(page)
+    # try:
+    #     paper_titles = get_paper_titles(page,keywords)
+    # except:
+    #     print("no document")
+    #     exit(1)
+    # else:
+    #     for title in paper_titles:
+    #         print(title)
